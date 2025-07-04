50c58cb750e9bf0d642a4b43fa965b13011e7b05
Adarsha Rimal <aagamers93@gmail.com>
2025-07-04 23:31:39 +0545

    ollama inference time

--- langchain/langchain-models/2.ChatModels/chatmodel_ollama_llama3.py
+++ langchain/langchain-models/2.ChatModels/chatmodel_ollama_llama3.py
@@ -3,5 +3,5 @@ from langchain_ollama import ChatOllama
 
 llm = ChatOllama(model="llama3")
 
-response = llm.invoke("What is the Nepali name of Mount Everest?")
+response = llm.invoke("Udemy business model")
 print(response.content)
