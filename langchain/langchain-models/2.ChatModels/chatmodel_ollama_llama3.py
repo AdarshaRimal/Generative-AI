a763248ed4787c1f47fce49c0f4ed7c30923a4c4
Adarsha Rimal <aagamers93@gmail.com>
2025-07-20 22:15:09 +0545

    learned about streaming llm response like chatgpt

--- langchain/langchain-models/2.ChatModels/chatmodel_ollama_llama3.py
+++ langchain/langchain-models/2.ChatModels/chatmodel_ollama_llama3.py
@@ -1,7 +1,9 @@
 # using locally available llam3
 from langchain_ollama import ChatOllama
 
-llm = ChatOllama(model="llama3")
+llm = ChatOllama(model="deepseek-r1",streaming = True)
+for chunk in llm.stream('cristiano ronaldo'):
+    print(chunk.content)
 
-response = llm.invoke("Udemy business model")
-print(response.content)
+# response = llm.invoke("Udemy business model")
+# print(response.content)
